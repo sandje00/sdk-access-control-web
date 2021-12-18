c010ab2944aca15d89289a7c05b2e62491c37fbe
sandje00 <stella.andjelic.00@fesb.hr>
2021-12-18 20:12:24 +0100

    Add login form

---
+++ src/components/Login.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="login position-center flex justify-center align-center">
+    <form class="login-form flex-v justify-start align-stretch">
+      <input
+        v-model="email"
+        type="text"
+        label="E-mail"
+      >
+      <input
+        v-model="password"
+        type="password"
+        label="Password"
+      >
+      <button
+        color="primary"
+        depressed>
+        Log In
+      </button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'login-page',
+  data: () => ({
+    email: '',
+    password: ''
+  })
+}
+</script>
+
+<style scoped lang="scss">
+.login {
+  background-color: var(--color-white);
+  width: 400px;
+  height: 400px;
+
+  &-form {
+    padding: 2rem;
+  }
+}
+</style>
