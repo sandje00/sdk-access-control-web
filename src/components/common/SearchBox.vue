56ffbacf2e806fa61542ffc8bb5a4e0314f8d65a
sandje00 <stella.andjelic.00@fesb.hr>
2022-01-09 17:58:18 +0100

    Add search box

---
+++ src/components/common/SearchBox.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="search-box flex-h align-center">
+    <icon-search icon-name="Search" icon-color="#acacac"></icon-search>
+    <base-input v-on="$listeners" v-bind="$attrs"></base-input>
+  </div>
+</template>
+
+<script>
+import BaseInput from './BaseInput';
+import IconSearch from './icons/IconSearch';
+
+export default {
+  name: 'search-box',
+  components: { BaseInput, IconSearch }
+}
+</script>
+
+<style lang="scss" scoped>
+.search-box {
+  width: 15rem;
+  height: 2rem;
+  padding: 0.5rem;
+  background-color: var(--color-white);
+  border: none;
+  border-radius: 10px;
+}
+</style>
--- src/components/dashboard/index.vue
+++ src/components/dashboard/index.vue
@@ -1,6 +1,11 @@
 <template>
   <app-layout logged-in>
     <div class="dashboard">
+      <search-box
+        v-model="keyword"
+        placeholder="Search"
+      >
+      </search-box>
       <dashboard-table :records="records">
       </dashboard-table>
     </div>
@@ -11,18 +16,20 @@
 import AppLayout from '../common/layout';
 import DashboardTable from './DashboardTable';
 import employeeData from '../../data/employees.json';
+import SearchBox from '../common/SearchBox';
 
 export default {
   name: 'admin-dashboard',
   data: () => ({
-    date: '2022-01-07'
+    date: '2022-01-07',
+    keyword: ''
   }),
   computed: {
     records() {
       return employeeData.find(it => it.date === this.date).records;
     }
   },
-  components: { AppLayout, DashboardTable }
+  components: { AppLayout, DashboardTable, SearchBox }
 }
 </script>
 
