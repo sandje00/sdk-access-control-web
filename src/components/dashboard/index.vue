495b715553b3aac26d2eb75a427c3e22532c183b
sandje00 <stella.andjelic.00@fesb.hr>
2022-02-05 18:20:25 +0100

    Add proper dashboard cotrol functionality

--- src/components/dashboard/index.vue
+++ src/components/dashboard/index.vue
@@ -1,16 +1,11 @@
 <template>
   <app-layout logged-in>
     <div class="dashboard">
-      <div class="dashboard-control">
-        <date-picker v-model="date">
-        </date-picker>
-        <search-box
-          v-model="keyword"
-          class="search"
-          placeholder="Search"
-        >
-        </search-box>
-      </div>
+      <dashboard-control
+        @date-change="loadRecords"
+        @keyword-change="filterRecords"
+      >
+      </dashboard-control>
       <dashboard-table :records="visibleRecords">
       </dashboard-table>
     </div>
@@ -19,43 +14,33 @@
 
 <script>
 import AppLayout from '../common/layout';
-import DatePicker from '../common/DatePicker';
+import DashboardControl from './DashboardControl';
 import DashboardTable from './DashboardTable';
 import employeeData from '../../data/employees.json';
-import SearchBox from '../common/SearchBox';
+import { getToday } from '../../utils/date';
 
 export default {
   name: 'admin-dashboard',
   data: () => ({
-    mockDate: '2022-01-07',
-    date: '',
-    keyword: '',
-    records: []
+    records: [],
+    visibleRecords: []
   }),
-  computed: {
-    visibleRecords() {
-      return this.keyword
-        ? this.filterRecords()
-        : this.records;
-    }
-  },
   created() {
-    this.records = employeeData.find(it => it.date === this.mockDate).records;
+    this.loadRecords(getToday());
   },
   methods: {
-    filterRecords() {
-      return this.records.filter(it =>
-        it.firstName.toUpperCase().includes(this.keyword.toUpperCase())
-          || it.lastName.toUpperCase().includes(this.keyword.toUpperCase())
+    loadRecords(date) {
+      this.records = employeeData.find(it => it.date === date).records;
+      this.visibleRecords = this.records;
+    },
+    filterRecords(keyword) {
+      this.visibleRecords = this.records.filter(it =>
+        it.firstName.toUpperCase().includes(keyword.toUpperCase())
+          || it.lastName.toUpperCase().includes(keyword.toUpperCase())
       );
     }
   },
-  components: {
-    AppLayout,
-    DashboardTable,
-    SearchBox,
-    DatePicker
-  }
+  components: { AppLayout, DashboardControl, DashboardTable }
 }
 </script>
 
@@ -67,22 +52,5 @@ export default {
   position: relative;
   left: 50%;
   transform: translateX(-50%);
-
-  &-control {
-    padding: 2rem 0;
-    display: flex;
-    justify-content: space-between;
-
-    @media(max-width: 650px) {
-      flex-direction: column;
-      align-items: stretch;
-    }
-  }
-
-  .search {
-    @media(max-width: 650px) {
-      margin-top: 1rem;
-    }
-  }
 }
 </style>
